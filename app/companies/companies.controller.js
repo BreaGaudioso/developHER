199ec0fe8d1200bdc763c1f2e31dfba64e37e768
Brea Gaudioso <breagaudioso@gmail.com>
2015-11-23 15:54:20 -0800

    charts

--- app/companies/companies.controller.js
+++ app/companies/companies.controller.js
@@ -14,6 +14,8 @@
         $scope.femaleDevs = parseInt(company.num_female_eng);
         $scope.allDevs = parseInt(company.num_eng);
         $scope.company = company.company;
+        $scope.percentage = ($scope.femaleDevs / $scope.allDevs)*100;
+        console.log($scope.percent);
         $scope.maleDevs = $scope.allDevs - $scope.femaleDevs;
         $scope.pollData = [$scope.femaleDevs, $scope.maleDevs]
         $scope.pollLabels = ["Female Developers", "Non-female Developers"]
